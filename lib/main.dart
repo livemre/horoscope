8f0e48e45a2943b1be2ec14984a7eda3a3a82360
Emre Karaduman <qaradumanemre@gmail.com>
2020-08-30 17:33:05 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:burc_rehberi/burc_liste.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Burclar",
+      home: BurcListesi(),
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+    );
+  }
+}
+
